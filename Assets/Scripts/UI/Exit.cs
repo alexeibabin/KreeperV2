7969afef3e3183dbe4f135db7e9ce6ca53de60c6
Daniel Gelbshtein <danielg@firstofferz.com>
2015-05-26 22:15:07 +0300

    UI scripts
    Recreated missing UI scripts

---
+++ Assets/Scripts/UI/Exit.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Exit : MonoBehaviour {
+
+	public void Activate(){
+		Application.Quit();
+	}
+}
---
+++ Assets/Scripts/UI/GazeOn.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GazeOn : MonoBehaviour {
+
+	public Transform target;
+	
+	private Transform localTransform;
+	
+	void Start(){
+		localTransform = transform;
+	}
+
+	void LateUpdate(){
+		if (target){
+			localTransform.LookAt(target.position);
+		}
+	}
+}
