5b2f2a04ec1b28cccebfa55bcb1b56894c44d997
Alexei Babin <alexei@moonactive.com>
2015-05-29 09:30:22 +0300

    Added menu sight controller

---
+++ Assets/Scripts/MenuSightController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuSightController : MonoBehaviour {
+
+	private CardboardHead playerHead;
+
+
+	// Use this for initialization
+	void Start () {
+		playerHead = GetComponentInChildren<CardboardHead>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
